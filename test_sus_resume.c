8544b773bbb58d363a35d00874ae1e9be88e4d38
Luke Mclaren <luke.patrick.mclaren@gmail.com>
2016-03-10 18:37:56 -0800

    Updated sus test to work without isr

--- test_sus_resume.c
+++ test_sus_resume.c
@@ -1,61 +1,46 @@
 #include "os.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
 
-unsigned int count = 0;
+#define F_CPU 16000000UL
+#include <util/delay.h>
 
 void Task_2(){
   for(;;){
     PORTB = 0x20;
-  }
-}
-
-void Task_1()
-{
-  for(;;){
-    PORTB = 0x40;
-  }
-}
-
-ISR(TIMER1_COMPA_vect){
-		count++;
-		if(count == 6) {
-			Task_Suspend(2);
-		}
-		if(count == 12) {
-			Task_Resume(2);
-			count = 0;
-		}
+    _delay_ms(100);
+    PORTB = 0x00;
+    _delay_ms(100);
     Task_Yield();
-    PORTB = 0x80;
+  }
+}
 
+void Task_1(){
+  unsigned int count = 0;
+
+  while(1){
+    count++;
+    PORTB = 0x40;
+    _delay_ms(100);
+    PORTB = 0x00;
+    _delay_ms(100);
+
+    if(!(count%8)){
+      Task_Suspend(2);
+    }else if(!(count%4)){
+      Task_Resume(2);
+    }
+
+    Task_Yield();
+  }
 }
 
 int main() {
 	DDRB = 0xF0;
 	PORTB = 0x00;
 
-	OS_Init();
+  OS_Init();
   Task_Create(Task_1, 0x00, 0x00);
   Task_Create(Task_2, 0x00, 0x00);
-
-  //Clear timer config.
-  TCCR1A = 0;
-
-  //Set to CTC (mode 4)
-  TCCR1B |= (1<<WGM12);
-
-  //Set prescaler to 256
-  TCCR1B |= (1<<CS12);
-
-  //Set TOP value (0.01 seconds)
-  OCR1A = 6250;
- 
-  //Enable interupt A for timer 3.
-  TIMSK1 |= (1<<OCIE1A);
-  //Set timer to 0 (optional here).
-  TCNT1 = 0;
-
   OS_Start();
 }
