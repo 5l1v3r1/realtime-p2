23011f37c7eeebb7fe061a26837f6b60bb930804
Luke Mclaren <luke.patrick.mclaren@gmail.com>
2016-03-14 11:01:02 -0700

    Confirming Mutex test for correctness, added explanation

--- test_mutex.c
+++ test_mutex.c
@@ -5,53 +5,61 @@
 #define F_CPU 16000000UL
 #include <util/delay.h>
 
+void Task_2();
 MUTEX shared_mutex;
 
 void Task_1(){
+  //12. Finally run Task_1's blink LED pin 11 
   PORTB = 0x20;
   _delay_ms(200);
   PORTB = 0x00;
   _delay_ms(200);
-  Task_Yield();
+
+  //13. Create Task_2 so loop continues at step 1 again.
+  Task_Create(Task_2, 3, 0);
 }
 
 void Task_3(){
-  //Lock locked mutex, should block Task 3 and pass priority to Task 2
+  //7. Lock locked mutex, should block Task 3 and Task 2 should inherit priority
+  //   after blocking on mutex lock, dispatches to next highest priority task
+  //   which will now be Task_2 since it has inherited Task_3's priority
   Mutex_Lock(shared_mutex);
+
+  //10. Once Mutex has been released blink the LED on pin 10
   PORTB = 0x10;
   _delay_ms(200);
   PORTB = 0x00;
   _delay_ms(200);
+
+  //11. Unlock the Mutex for completeness
   Mutex_Unlock(shared_mutex);
-  Task_Yield();
 }
 
 void Task_2(){
-  //Lock the shared mutex, ensures that task_2 runs first due to priority inheritence
+  //3. Lock the shared mutex
   Mutex_Lock(shared_mutex);
 
-  //Create Task_3 
-  int Task_3_id = Task_Create(Task_3, 1, 0); // Pin 12
-  //Yield to task 3
+  //4. Create Task_3 with higher priority than Task_2
+  int Task_3_id = Task_Create(Task_3, 1, 0);
+
+  //5. Create Task_2 with higher priority than Task_2 but lower Priority than Task_3
+  int Task_2_id = Task_Create(Task_1, 2, 0);
+  
+  //6. Yield to Task_3
   Task_Yield();
 
-  //Create Task_2 
-  int Task_2_id = Task_Create(Task_1, 2, 0); // Pin 12
-  //Task should not yield to task 2 since it has inherited a higher priority from task 3
-  Task_Yield();
-
+  //8. Return from Task_3 here and blink pin 13 LED
   PORTB = 0x80;
   _delay_ms(200);
   PORTB = 0x00;
   _delay_ms(200);
 
-  //Unlock the mutex
+  //9. Unlock the Mutex ->  unblocking Task_3
   Mutex_Unlock(shared_mutex);
-  
-  //Yield should send to task 3
-  Task_Yield();
 }
 
+
+// OLD BUG but good explanation why it is bad to use the number of tasks as the ID
 // after this terminates any new tasks will have overlapping id's
 // example
 // a_main == id == tasks == 0
@@ -63,6 +71,9 @@ void a_main() {
   DDRB = 0xF0;
   PORTB = 0x00;
 
+  // 1. Create the shared mutex
   shared_mutex = Mutex_Init();
+
+  // 2. Create the first task to run aka Task_2, with the lowest priority
   Task_Create(Task_2, 3, 0);
 }
