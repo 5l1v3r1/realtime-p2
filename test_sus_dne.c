5683e40877ca0cc95845499e5f410cb976bb9af2
pascale-m <lilp@Pascales-MacBook-Pro.local>
2016-03-14 09:14:00 -0700

    Added test_sus_dne.c

---
+++ test_sus_dne.c
@@ -0,0 +1,20 @@
+/* Tests if system crashes when a non existant process is suspended or resumed */
+
+#include "os.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+unsigned int does_not_exist = 0;
+
+void Task_1() {
+	Task_Suspend(does_not_exist);
+
+	Task_Resume(does_not_exist);
+}
+
+
+int a_main() {
+	Task_Create(Task_1, 0, 0);
+
+	Task_Terminate();
+}
